dc7847b71f5939a5bdbe41268a90a9cc4c02cf5c
Sholto <madein.mania@gmail.com>
2021-10-14 09:48:36 +0100

    route register post

--- routes/index.js
+++ routes/index.js
@@ -6,5 +6,6 @@ router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
 router.get('/register', registerController.get);
+router.post('/register', registerController.post);
 
 module.exports = router;
