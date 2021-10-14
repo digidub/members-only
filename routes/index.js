8ce7171ec590dbd08a0ba76a9583ad995c437040
Sholto <madein.mania@gmail.com>
2021-10-14 09:21:36 +0100

    register router

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const registerController = require('../controllers/registerController');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
+router.get('/register', registerController.get);
 
 module.exports = router;
