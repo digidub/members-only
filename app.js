9aa60e504f1ef01eb9e4e83e6da6406282af2224
Sholto <madein.mania@gmail.com>
2021-10-14 09:37:24 +0100

    passport config and init

--- app.js
+++ app.js
@@ -51,4 +51,33 @@ app.use(function (err, req, res, next) {
   res.render('error');
 });
 
+passport.use(
+  new LocalStrategy((username, password, done) => {
+    User.findOne({ username: username }, (err, user) => {
+      if (err) {
+        return done(err);
+      }
+      if (user.password !== password) {
+        return done(null, false, { message: 'Your credentials are INVALID' });
+      }
+      return done(null, user);
+    });
+  })
+);
+
+passport.serializeUser(function (user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function (id, done) {
+  User.findById(id, function (err, user) {
+    done(err, user);
+  });
+});
+
+app.use(session({ secret: 'cats', resave: false, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(express.urlencoded({ extended: false }));
+
 module.exports = app;
