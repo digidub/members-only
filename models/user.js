4fd76187fdf7af3a792fd5372d2c5aa4475ed250
Sholto <madein.mania@gmail.com>
2021-10-14 08:48:47 +0100

    user schema

---
+++ models/user.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const validator = require('validator');
+
+const UserSchema = new Schema({
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    validate: [validator.isEmail, 'please enter a valid email address'],
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  firstName: {
+    type: String,
+    required: true,
+    minLength: 2,
+    maxLength: 50,
+  },
+  lastName: {
+    type: String,
+    required: true,
+    minLength: 2,
+    maxLength: 50,
+  },
+  membershipStatus: {
+    type: String,
+    default: 'unconfirmed',
+    enum: ['unconfirmed', 'confirmed'],
+  },
+});
