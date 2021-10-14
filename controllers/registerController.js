2e844bf8abb7b9aeea868acbee9b5044ab65958e
Sholto <madein.mania@gmail.com>
2021-10-14 09:32:21 +0100

    register controller module

--- controllers/registerController.js
+++ controllers/registerController.js
@@ -2,6 +2,6 @@ const User = require('../models/user');
 const Message = require('../models/message');
 const { body, validationResult } = require('express-validator');
 
-exports.register = async (req, res) => {
-  // logic to handle registration page here
+exports.get = async (req, res) => {
+  res.render('register');
 };
