dd2afe5df8a5c725c5b2599c97531146374c27be
Sholto <madein.mania@gmail.com>
2021-10-16 11:34:24 +0100

    added redirect if user not logged in

--- controllers/memberAreaController.js
+++ controllers/memberAreaController.js
@@ -3,7 +3,14 @@ const User = require('../models/user');
 exports.get = async (req, res, next) => {
   console.log(res.locals.currentUser);
 
+  if (!res.locals.currentUser) {
+    res.redirect('/');
+    next();
+  }
+
   if (res.locals.currentUser.membershipStatus !== 'confirmed') {
     res.render('member-area', { confirmed: false });
   }
 };
+
+exports.post = async (req, res, next) => {};
