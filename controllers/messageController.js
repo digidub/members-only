5863185906ea2a1934e74a47fae963d1f0d34750
Sholto <madein.mania@gmail.com>
2021-10-20 10:37:15 +0100

    message post logic

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -1,5 +1,29 @@
 const Message = require('../models/message');
+const { body, validationResult } = require('express-validator');
 
-exports.messagePost = async function (req, res, next) {
+exports.postMessageGet = async function (req, res, next) {
   res.render('message-post');
+};
+
+exports.postMessagePost = async function (req, res, next) {
+  body('subject', 'Subject must not be empty')
+    .trim()
+    .isLength({ min: 3 })
+    .escape();
+  body('message').trim().isLength({ min: 10 }).escape();
+  async (req, res, next) => {
+    const errors = validationResult(req);
+
+    const message = new Message({
+      subject: req.body.subject,
+      message: req.body.message,
+    });
+
+    if (!errors.isEmpty()) {
+      res.render('message-post', { subject, message });
+    } else {
+      await message.save();
+      res.redirect('/messages');
+    }
+  };
 };
