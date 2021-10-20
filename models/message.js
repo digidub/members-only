d556f2ae84d19c232d910c7fc79200dfc766a424
Sholto <madein.mania@gmail.com>
2021-10-20 11:30:47 +0100

    updated schema to match controllers and views

--- models/message.js
+++ models/message.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const MessageSchema = new Schema({
-  title: { type: String, required: true, minLength: 3, maxLength: 50 },
-  body: { type: String, required: true, minLength: 10, maxLength: 560 },
+  subject: { type: String, required: true, minLength: 3, maxLength: 50 },
+  message: { type: String, required: true, minLength: 10, maxLength: 560 },
   timestamp: { type: Date, default: Date.now },
   author: { type: Schema.Types.ObjectId, ref: 'User' },
 });
